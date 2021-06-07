c9afb20f61fca205f32b354acd6c353978270472
Rick Brown <ricky.brown.00@gmail.com>
2021-06-07 20:01:03 +0100

    Initial Commit

---
+++ SwiftUI_Photo_Editor/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_Photo_Editor
+//
+//  Created by Rick Brown on 07/06/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI_Photo_Editor/SwiftUI_Photo_EditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Photo_EditorApp.swift
+//  SwiftUI_Photo_Editor
+//
+//  Created by Rick Brown on 07/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Photo_EditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
